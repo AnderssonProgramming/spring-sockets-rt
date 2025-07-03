a77bd864ff5be9a0a034d1c25caf5b48c29b8b6d
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-07-03 11:30:20 -0500

    feat: add spring websockets react project scaffolding

---
+++ src/main/java/edu/eci/arsw/spring_sockets_rt/App.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.spring_sockets_rt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
